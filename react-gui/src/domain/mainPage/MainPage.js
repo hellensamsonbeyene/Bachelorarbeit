01ee6e92a70fa56120a89d9e87bc2322260153c5
hellen.samson-beyene <hellen.samson-beyene@adesso.de>
2024-04-18 12:03:43 +0200

    Tablet Version hinzugefügt, es wird dann zurückgesetzt auf den ZEKI Chatbot

--- react-gui/src/domain/mainPage/MainPage.js
+++ react-gui/src/domain/mainPage/MainPage.js
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 //mui
 import Snackbar from "@mui/material/Snackbar";
 import Alert from '@mui/material/Alert';
 // custom
 import NavigationBar from "../../NavigationBar/NavigationBar";
 import CustomChatbot from "../../components/CustomChabot/CustomChatbot";
+import VisualChatbot from "../../components/VisualChatbot/VisualChatbot";
+import ChatbotService from "../../services/ChatbotService";
 
 // default colors
 const themeChatbot = {
@@ -23,27 +25,53 @@ const MainPage = () => {
     const [showPopUp, setShowPopUp] = useState(false);
     const [popUpMessage, setPopUpMessage] = useState("");
     const [colorPopUp, setColorPopUp] = useState("");
+    const [isMobile, setIsMobile] = useState(false)
+
+    //choose the screen size
+    const handleResize = () => {
+        if (window.innerWidth < 720) {
+            setIsMobile(true);
+            ChatbotService.resetChatbot()
+                .then(() => {
+                    setShowPopUp(true);
+                    setColorPopUp("success");
+                    setPopUpMessage("Chatbot erfolgreich zurückgesetzt!");
+                })
+        } else {
+            setIsMobile(false)
+        }
+    }
+
+    // create an event listener
+    useEffect(() => {
+        window.addEventListener("resize", handleResize)
+    })
 
     return (<>
-            <NavigationBar theme={themeChatbot}/>
-            <CustomChatbot theme={themeChatbot} setShowPopUp={setShowPopUp} setPopUpMessage={setPopUpMessage} setColorPopUp={setColorPopUp} />
-            <Snackbar
-                open={showPopUp}
-                anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'left',
-                }}
-                autoHideDuration={5000} // 3 Sekunden
-                onClose={() => { setShowPopUp(false) }}
-            >
-                <Alert
-                    severity={colorPopUp || "info"}
-                    variant="filled"
-                    sx={{ width: '100%' }}
-                >
-                    {popUpMessage}
-                </Alert>
-            </Snackbar>
+        {!isMobile && <><NavigationBar theme={themeChatbot}/>
+                        <CustomChatbot theme={themeChatbot} setShowPopUp={setShowPopUp} setPopUpMessage={setPopUpMessage} setColorPopUp={setColorPopUp}/>
+                        <Snackbar
+                            open={showPopUp}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            autoHideDuration={5000} // 3 Sekunden
+                            onClose={() => {
+                                setShowPopUp(false)
+                            }}
+                        >
+                        <Alert
+                            severity={colorPopUp || "info"}
+                            variant="filled"
+                            sx={{width: '100%'}}
+                        >
+                            {popUpMessage}
+                        </Alert>
+                        </Snackbar>
+                    </>}
+            {isMobile&& <VisualChatbot theme={themeChatbot} setColorPopUp={setColorPopUp} setShowPopUp={setShowPopUp} setPopUpMessage={setPopUpMessage}/>
+            }
         </>
     );
 }
